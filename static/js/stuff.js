f8b1174929badaf8808bd8838da061f39075f063
Annie <awong3692@gmail.com>
2014-02-19 20:13:04 -0800

    Tickled Pink

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -2,22 +2,27 @@ alert("Welcome to Annie's Page!"); // edit me!
 
 // Problem 1 (Say Hello!) ---------------------------------------------------
 $('#say_hello').click(function() {
-	alert('Hello World!')
+	alert('Hello World!');
 });
 
 
 // Problem 2 (Houdini) ------------------------------------------------------
 $('#disappear').click(function() {
+	$('#disappear').hide();
   //WRITE CODE HERE
 });
 
 $('#reappear').click(function() {
+	$('#disappear').show();
   //WRITE CODE HERE
 });
 
 
 // Problem 3 (Tickle Me Pink) -----------------------------------------------
 // WRITE CODE HERE
+$('#tickled_pink').click(function(){
+	$('#tickled_text').css('color','Pink');
+});
 
 
 // Problem 4 (Greet Me) -----------------------------------------------------
