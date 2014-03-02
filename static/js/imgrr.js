fd3888fb601ee57b96ea3ae651b81dbe1e912e02
Annie <awong3692@gmail.com>
2014-03-01 19:07:30 -0800

    gorilla

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,6 +1,20 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
 // WRITE CODE HERE
+$('#ComeBack').hide();
+$('#GoAway').click(function() {
+	$('#GoAway').hide();
+	$('#main_img').hide();
+	$('#ComeBack').show();
+});
+$('#ComeBack').click(function() {
+	$('#ComeBack').hide();
+	$('#main_img').show();
+	$('#GoAway').show();
 
+	
+
+
+});
 // Problem 3 (Swap Em) -----------------------------------------------
 // WRITE CODE HERE
 
