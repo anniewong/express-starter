2e2f950925e1b4929fe5afdea1b8360024309bf1
Annie <awong3692@gmail.com>
2014-03-02 22:19:40 -0800

    imgrr up to Step 7

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,26 +1,74 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
 // WRITE CODE HERE
-$('#ComeBack').hide();
-$('#GoAway').click(function() {
-	$('#GoAway').hide();
-	$('#main_img').hide();
-	$('#ComeBack').show();
-});
-$('#ComeBack').click(function() {
-	$('#ComeBack').hide();
-	$('#main_img').show();
-	$('#GoAway').show();
+//$('#ComeBack').hide();
+/*
+if($("#GoAway").is(":visible")){
+	$('#GoAway').click(function() {
+			$('#GoAway').hide();
+			$('#main_img').hide();
+			$('#ComeBack').show();
+		}
+	});
+ else {
+	$('#ComeBack').click(function() {
+		$('#ComeBack').hide();
+		$('#main_img').show();
+		$('#GoAway').show();
+	});
+}
+*/
+$('#GorillaVisibility').click(function() {
+	if ($('#GorillaVisibility').text()==="Go Away!") {
+		$('#main_img').hide();
+		$('#GorillaVisibility').text("Come Back!");
 
+
+	} else{ 
+		$('#main_img').show();
+		$('#GorillaVisibility').text("Go Away!");
+	}
+		
 	
-
-
 });
+
+
+/*$('#ComeBack').hide();
+if($("#GorillaVisibility").text() === "Go Away!"){
+        $('#GorillaVisibility').click(function() {
+                      
+                        $('#main_img').hide();
+                        $('#ComeBack').show();
+        })
+/*} else { alert("Hello");
+        $('#G').click(function() {
+                $('#ComeBack').hide();
+                $('#main_img').show();
+                $('#GoAway').show();
+        });
+}
+*/
+
+
+
+
 // Problem 3 (Swap Em) -----------------------------------------------
 // WRITE CODE HERE
+function onchange_img() {
+	var textusertyped = $('#new_img_file').val();
+	var texttoshow = "/static/img/"+textusertyped;
+	$('#main_img').attr('src', texttoshow);
+}
+
+
+
+$('#change_img').click(onchange_img);
+
+
+
 
 // Problem 4 (Find the Source) -------------------------------------------------
 $('.clickable').click(function() {
-  // WRITE CODE HERE
+	alert($(this).attr('src'));
 });
 
 // Problem 5 (Imgrr) -------------------------------------------------
