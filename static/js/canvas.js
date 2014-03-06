40d3942fbc585388572b0c90a7f4f8c52d85cb8a
Annie <awong3692@gmail.com>
2014-03-05 19:44:31 -0800

    hurray all circles

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -12,14 +12,33 @@ $(document).ready(function() {
   //
 
   $('#p1').click(function(){
-  	context.strokeRect(10,20,10,20)});
+  	context.strokeRect(10,20,10,20)
+  });
+  
 
 $('#p2').click(function(){
-	context.strokeRect(30,30,30,30)});
+	context.strokeRect(30,30,30,30)
 });
 
 
+$('#p3').click(function(){
+context.beginPath();
+context.arc(200,150,125,2,4);
+context.stroke ()
+});
 
+$('#p4').click(function(){
+  context.beginPath();
+  context.arc(300,200,150,3,5*Math.PI);
+  context.stroke()
+});
 
+$('#p5').click(function(){
+  context.moveTo (15,45);
+  context.lineTo(80,20);
+  context.strokeStyle = 'black';
+  context.stroke();
+  });
 
+});
  
