7c90cdf217d0abdecc712fbba201c675fb3fa1d7
Annie <awong3692@gmail.com>
2014-03-12 19:39:31 -0700

    Mastered circles and squares!

---
+++ static/js/funcs.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+  //this is how we acquire control of the canvas
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext("2d");
+
+  $('#clear').click(function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  var drawSquare = function(x, y, sideLen, color) {
+    //alert('Drawing a square is unimplemented!');  
+    context.strokeStyle = color;
+    context.strokeRect(x, y, sideLen, sideLen);
+  };
+
+
+    // Write square drawing code here
+    // Delete the alerts when done
+  
+
+  var drawCircle = function(x, y, radius, color) {
+    //alert('Drawing a circle is unimplemented!');
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2*Math.PI);
+    context.strokeStyle = color;
+    context.closePath();
+    context.stroke();
+    // Write circle drawing code here
+    // Delete the alert when done
+  };
+
+
+  // Write drawTriplet function here
+
+  // Challenge:
+  // Write drawTriangle, drawTriforce, drawTripleTriforce,
+  // drawSierpinski functions here
+
+  $('#p1').click(function() {
+    drawSquare(100, 200, 50, 'blue');
+  });
+
+  $('#p2').click(function() {
+    drawSquare(300, 100, 25, 'green');
+  });
+
+  $('#p3').click(function() {
+    drawCircle(100, 200, 50, 'red');
+  });
+
+  $('#p4').click(function() {
+    drawCircle(300, 100, 25, 'black');
+  });
+
+  //---------------------------------------------------------------------------
+  //Write your code for p5-p11 here
+  //
+
+  $('#p5').click(function() {
+    drawSquare(200,200,100, 'red');
+    drawCircle(250,250,50, 'blue');
+    drawCircle(250,200,50, 'green');
+    drawCircle(200,250,50, 'green');
+    drawCircle(250,300,50, 'green');
+    drawCircle(300,250,50, 'green');
+
+
+
+    
+  });
+
+});
